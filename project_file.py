93a72e7aab0f8d04de31d1df5589aacd078e574e
codingadventurestoday <97482585+codingadventurestoday@users.noreply.github.com>
2022-08-22 13:57:40 -0700

    Update project_file.py
    Created the helper function that is used to scrap the contents of a website and turn it into a beautifulsoup object to be parsed through.

--- project_file.py
+++ project_file.py
@@ -1,5 +1,5 @@
-import urllib.request 
-import beautifulSoup 
+import requests 
+from bs4 import BeautifulSoup 
 
 # this dict stores apprenticeship companies names as keys and the urls used to look up availability as the values
 apprenticeship_opportunity = {'Barclays': 'https://search.jobs.barclays/foundation-apprenticeships', 
@@ -11,19 +11,19 @@ apprenticeship_opportunity = {'Barclays': 'https://search.jobs.barclays/foundati
                               'Twitch': 'https://www.twitch.tv/jobs/en/early-career/#apprentice'
                               }
 
-
-
-
-# this function is used to create a beautiful soup object for each url give 
-def bbsObject(url):
-    link = urllib.request(url) # look up the correct method for this 
-
-    return 
-#this function converts the bbsobject into the a json structure 
-def jsonStructure(bbsObject):
-
-    return 
-
-# this function takes the json structure and prints the infomation related to the apprenticeship
-def relatedInformation(company, jsonStructure):
-    print(apprenticeship_opportunity[company]':')
+#function that creates a beautifulsoup obj as in ouput. which we can parse through. Input is the company's name which website's will be scraped
+#first company_webpage is set to the http which is the value to the companies name key in the apprenticeship_opportunity dict
+#Second web_response is set to the webpage's HTML with the requests library's get function and content function
+#third bsObj is set to the a BeautifulSoup object with the content of the website's HTML then returned
+def create_bs_obj(company_name):
+    try:
+        company_webpage = apprenticeship_opportunity[company_name]
+        web_response = requests.get(company_webpage).content
+        bsObj = BeautifulSoup(web_response)
+        return bsObj
+    except: 
+        print(f'''There is a problem with creating a BeauitfulSoup object with {company_name}. The problems may be: 
+        1. The company is not in our apprenticeship_opportunity dictionary. Solution is to add the company and it's website. 
+        2. The company's website address has changed. Please ensure the http is correct.
+        3. The script could not connect to the server. Ensure that you are connected to the internet. Then try again.'''
+        return None 
